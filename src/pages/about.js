4667c59ee779eaa4254ff0ebefa1c1a19a00da64
irrellia <dicha.arkana03@gmail.com>
2020-01-31 21:24:55 +0700

    Added title for about and archive page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 function AboutPage() {
   return (
     <Layout>
+      <SEO title="About" />
       <h1>Hi, this is about page</h1>
     </Layout>
   )
--- src/pages/archives.js
+++ src/pages/archives.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 function ArchivesPage() {
   return (
     <Layout>
+      <SEO title="Archives" />
       <h1>Hi, this is archives page</h1>
     </Layout>
   )
