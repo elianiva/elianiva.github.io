bc6f3e2fe09d149b8f595aaae78656b8789f131a
irrellia <dicha.arkana03@gmail.com>
2020-01-31 18:43:27 +0700

    Added about and archive pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+function AboutPage() {
+  return (
+    <Layout>
+      <h1>Hi, this is about page</h1>
+    </Layout>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/archives.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+function ArchivesPage() {
+  return (
+    <Layout>
+      <h1>Hi, this is archives page</h1>
+    </Layout>
+  )
+}
+
+export default ArchivesPage
