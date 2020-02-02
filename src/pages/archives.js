173a8e122b8258af027f4ff08b931cdb610b8604
irrellia <dicha.arkana03@gmail.com>
2020-02-02 17:48:55 +0700

    Partially finished archives page

--- src/pages/archives.js
+++ src/pages/archives.js
@@ -1,12 +1,22 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Category from "../components/category"
+import Styles from "../styles/archives.module.css"
 
 function ArchivesPage() {
   return (
     <Layout>
       <SEO title="Archives" />
-      <h1>Hi, this is archives page</h1>
+      <div className={Styles.container}>
+        <h1>ARCHIVES</h1>
+        <hr />
+        <span className={Styles.subtitle}>CATEGORIES</span>
+        <Category category="linux" number="2" />
+        <Category category="neovim" number="12" />
+        <Category category="webdev" number="3" />
+        <Category category="others" number="5" />
+      </div>
     </Layout>
   )
 }
