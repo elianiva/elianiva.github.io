3506c8ae9a06b75d3e0343a6d56d68e9e653425c
irrellia <dicha.arkana03@gmail.com>
2020-02-08 13:28:28 +0700

    Changed navbar max-width

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,37 +7,39 @@ function Navbar() {
   return (
     <>
       <div className={Styles.container}>
+        <div className={Styles.wrapper}>
+          <div
+            className={Styles.logo}
+            style={isOpen ? { opacity: 0 } : { opacity: 1 }}
+          >
+            <Link to="/">IRRELLIA</Link>
+          </div>
+          <div className={Styles.hamburger}>
+            <input type="checkbox" onClick={() => setOpen(!isOpen)} />
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
         <div
-          className={Styles.logo}
-          style={isOpen ? { opacity: 0 } : { opacity: 1 }}
+          className={Styles.overlay}
+          style={
+            isOpen
+              ? { display: "block", opacity: 1 }
+              : { visibility: "none", pointerEvents: "none", opacity: 0 }
+          }
         >
-          <Link to="/">IRRELLIA</Link>
-        </div>
-        <div className={Styles.hamburger}>
-          <input type="checkbox" onClick={() => setOpen(!isOpen)} />
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-      <div
-        className={Styles.overlay}
-        style={
-          isOpen
-            ? { display: "block", opacity: 1 }
-            : { visibility: "none", pointerEvents: "none", opacity: 0 }
-        }
-      >
-        <div className={Styles.items}>
-          <span className={Styles.item} tabIndex="-1">
-            <Link to="/">HOME</Link>
-          </span>
-          <span className={Styles.item} tabIndex="-1">
-            <Link to="/archives">ARCHIVES</Link>
-          </span>
-          <span className={Styles.item} tabIndex="-1">
-            <Link to="/about">ABOUT</Link>
-          </span>
+          <div className={Styles.items}>
+            <span className={Styles.item} tabIndex="-1">
+              <Link to="/">HOME</Link>
+            </span>
+            <span className={Styles.item} tabIndex="-1">
+              <Link to="/archives">ARCHIVES</Link>
+            </span>
+            <span className={Styles.item} tabIndex="-1">
+              <Link to="/about">ABOUT</Link>
+            </span>
+          </div>
         </div>
       </div>
     </>
