bf612e6d2f7306e0390d42bf57901c82acfe1849
irrellia <dicha.arkana03@gmail.com>
2020-04-27 13:15:09 +0700

    removed social icon from footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,59 +1,12 @@
 import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
 import Styles from "../styles/footer.module.css"
-import Facebook from "../assets/facebook.svg"
-import Reddit from "../assets/reddit.svg"
-import Twitter from "../assets/twitter.svg"
-import Github from "../assets/github.svg"
 
 function Footer() {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          facebook
-          reddit
-          twitter
-          github
-        }
-      }
-    }
-  `)
   return (
     <div className={Styles.container}>
       <div className={Styles.wrapper}>
         <div className={Styles.sideWrap}>
           <span className={Styles.findme}>FIND ME ON</span>
-          <div className={Styles.icons}>
-            <a
-              href={data.site.siteMetadata.facebook}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Facebook className={Styles.icon} fill="#292d3e" />
-            </a>
-            <a
-              href={data.site.siteMetadata.reddit}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Reddit className={Styles.icon} fill="#292d3e" />
-            </a>
-            <a
-              href={data.site.siteMetadata.twitter}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Twitter className={Styles.icon} fill="#292d3e" />
-            </a>
-            <a
-              href={data.site.siteMetadata.github}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Github className={Styles.icon} fill="#292d3e" />
-            </a>
-          </div>
         </div>
         <span className={Styles.desc}>
           This site is built using{" "}
