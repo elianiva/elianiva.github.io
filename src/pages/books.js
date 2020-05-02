a8b76268506efe6f3d788f380c32520b9bd5a544
irrellia <dicha.arkana03@gmail.com>
2020-05-02 15:06:31 +0700

    added new books page (WIP)

---
+++ src/pages/books.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+function Books() {
+  return (
+    <Layout>
+      <SEO url="https://elianiva.github.io/books" title="Books" />
+      <h1 style={{ textAlign: "center", marginTop: `2rem` }}>Coming Soon</h1>
+    </Layout>
+  )
+}
+
+export default Books
