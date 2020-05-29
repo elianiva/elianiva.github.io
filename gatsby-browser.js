6e565d9efbac8777bc981a7a92c2751c587b1f34
irrellia <dicha.arkana03@gmail.com>
2020-05-29 15:07:28 +0700

    improved font look and performance using self hosted fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,10 @@
+// lazyload images
 import "lazysizes"
+
+// fonts
+import "typeface-pt-sans"
+import "typeface-satisfy"
+import "typeface-roboto-condensed"
+
+// prism.js highlight theme
 require("./src/styles/prism-theme.css")
--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import "./layout.css"
+import "../styles/layout.css"
 
 import Navbar from "./navbar"
 import Footer from "./footer"
